71501ba9290b5c953f57655356b5da39c5d12830
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-20 19:21:42 +0530

    [Arun] Added welcome message program

---
+++ LineComparison/LineComparison.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparison
+{
+    internal class LineComparison
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison problem!");
+        }
+    }
+}

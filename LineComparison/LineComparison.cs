7d51987321f686a480ad6cfbd1befcb335c4cf37
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-20 20:53:08 +0530

    [Arun] Added code to check equality of two lines

--- LineComparison/LineComparison.cs
+++ LineComparison/LineComparison.cs
@@ -1,11 +1,12 @@
-﻿namespace LineComparison
+﻿using System.Buffers.Text;
+
+namespace LineComparison
 {
     internal class LineComparison
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to line comparison problem!");
 
+        static int LineLengthCalculator()
+        {
             //UC1 Model a line based on a point consisting of (x,y) co-ordinates using the cartesian system, to calculate length.
             Console.WriteLine("Enter x1 and y1 values : ");
             int x1 = Convert.ToInt32(Console.ReadLine());
@@ -13,8 +14,33 @@
             Console.WriteLine("Enter x2 and y2 values : ");
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
-            int lengthOfLine1 = (int)(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of line 1 : "+lengthOfLine1);
+            int lengthOfLine = (int)(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return lengthOfLine;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison problem!");
+            int tracker = 0;
+
+            //UC2 Check equality of two lines based on the end points.
+            int[] twoLengths = new int[2];
+            for (int count = 0; count < twoLengths.Length; count++)
+            {
+                tracker++;
+                Console.WriteLine("\n" + tracker + " Line values .");
+                twoLengths[count] =  LineLengthCalculator();
+            }
+            Console.WriteLine("\nLength of Line 1 : " + twoLengths[0] + "\nLength of Line 2 : " + twoLengths[1]);
+            bool equals = twoLengths[0].Equals(twoLengths[1]);
+            if (equals == true)
+            {
+                Console.WriteLine("Both the lines are of equal length.");
+            }
+
+            else
+            {
+                Console.WriteLine("Lines are not of equal length.");
+            }
         }
     }
 }
